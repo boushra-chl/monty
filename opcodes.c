48bdd8711b8131113625e9ff818aecc637c128d4
boushra-chl <bchilouch1998@gmail.com>
2024-02-11 16:46:44 +0000

    pall fix

--- opcodes.c
+++ opcodes.c
@@ -39,11 +39,9 @@ void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = (*stack);
 
+	(void) line_number;
 	if (current == NULL)
-	{
-		fprintf(stderr, "L%d: usage: pall\n", line_number);
 		exit(EXIT_FAILURE);
-	}
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
