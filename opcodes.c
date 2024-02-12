eaae0425428fcde16cc4db409098ce1063e2194c
boushra-chl <bchilouch1998@gmail.com>
2024-02-12 15:46:03 +0000

    fix

--- opcodes.c
+++ opcodes.c
@@ -81,7 +81,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	stack = &head;
 	if ((*stack) == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	tmp = *stack;
