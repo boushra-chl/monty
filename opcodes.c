97c04008bfbf3db6ff765662a15e6fd4294bba62
boushra-chl <bchilouch1998@gmail.com>
2024-02-12 11:35:38 +0000

    fix pint error

--- opcodes.c
+++ opcodes.c
@@ -61,7 +61,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	stack = &head;
 	if ((*stack) == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack is empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
